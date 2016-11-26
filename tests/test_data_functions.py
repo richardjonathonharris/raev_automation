1f1638f8fa6c1944219c2ed90a9f5feae87a810b
Richard Harris <richard.jonathon.harris@gmail.com>
2016-11-26 11:38:02 -0600

    Added base data code and began testing suite

---
+++ tests/test_data_functions.py
@@ -0,0 +1,8 @@
+import json 
+from nose.tools import *
+
+def test_card_data_provides_valid_json():
+    testing_suite = ['action.json']
+    for file in testing_suite:
+        with open('data/%s' % file ) as data_file:
+            data = json.load(data_file)
