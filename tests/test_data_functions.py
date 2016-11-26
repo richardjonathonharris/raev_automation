716863b02f44670a6ddc3ef518291ed81cd14ce8
Richard Harris <richard.jonathon.harris@gmail.com>
2016-11-26 11:54:38 -0600

    Added gitignore, class and deployment functions

--- tests/test_data_functions.py
+++ tests/test_data_functions.py
@@ -2,7 +2,7 @@ import json
 from nose.tools import *
 
 def test_card_data_provides_valid_json():
-    testing_suite = ['action.json']
+    testing_suite = ['action.json', 'deployment.json', 'class.json']
     for file in testing_suite:
         with open('data/%s' % file ) as data_file:
             data = json.load(data_file)
