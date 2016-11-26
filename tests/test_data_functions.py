9d7518bbaa36f6d89113df0ebd4114f14c237557
Richard Harris <richard.jonathon.harris@gmail.com>
2016-11-26 12:20:07 -0600

    Typo and remove pyc files

--- tests/test_data_functions.py
+++ tests/test_data_functions.py
@@ -4,5 +4,7 @@ from nose.tools import *
 def test_card_data_provides_valid_json():
     testing_suite = ['action.json', 'deployment.json', 'class.json', 'agenda.json']
     for file in testing_suite:
-        with open('data/%s' % file ) as data_file:
+        with open('data/%s' % file) as data_file:
             data = json.load(data_file)
+
+
